6f30acdce4caf9d51a3dc464424f27af834855e5
Dinesh Kuberan <dinesh.kuberan@pearson.com>
2020-02-04 18:38:21 +0530

    Updates

---
+++ precache-manifest.2762338a27737b41d76cdba57a541f9c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06167c18003afbcb17aeac395242448e",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "b72b51f865c9ab44148a",
+    "url": "/react-shopping-cart/static/css/0.e717f0a3.chunk.css"
+  },
+  {
+    "revision": "b44446f9d48bdf3dd360",
+    "url": "/react-shopping-cart/static/css/4.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "ae90a2379c072981bead",
+    "url": "/react-shopping-cart/static/css/5.c56786bc.chunk.css"
+  },
+  {
+    "revision": "64570b0b4bb2777b7153",
+    "url": "/react-shopping-cart/static/css/6.f8f37e45.chunk.css"
+  },
+  {
+    "revision": "f3d1baa9f34bc39996a3",
+    "url": "/react-shopping-cart/static/css/main.c5377c4c.chunk.css"
+  },
+  {
+    "revision": "b72b51f865c9ab44148a",
+    "url": "/react-shopping-cart/static/js/0.a53df5bb.chunk.js"
+  },
+  {
+    "revision": "c9ef3f2a160e10bbde83",
+    "url": "/react-shopping-cart/static/js/3.31192375.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-shopping-cart/static/js/3.31192375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44446f9d48bdf3dd360",
+    "url": "/react-shopping-cart/static/js/4.d691c673.chunk.js"
+  },
+  {
+    "revision": "ae90a2379c072981bead",
+    "url": "/react-shopping-cart/static/js/5.6b8d68a0.chunk.js"
+  },
+  {
+    "revision": "64570b0b4bb2777b7153",
+    "url": "/react-shopping-cart/static/js/6.d250ff1b.chunk.js"
+  },
+  {
+    "revision": "f3d1baa9f34bc39996a3",
+    "url": "/react-shopping-cart/static/js/main.9091f58a.chunk.js"
+  },
+  {
+    "revision": "8192c32aaf22032cd88e",
+    "url": "/react-shopping-cart/static/js/runtime-main.71169357.js"
+  }
+]);
